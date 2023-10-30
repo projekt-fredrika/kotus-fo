de2141acaac04bc4cbdc29f54254f130c1648984
robertsilen <robert.silen@iki.fi>
2023-10-30 12:59:01 +0200

    kod för att skapa lexem

--- kotus-fo_create_lexem.py
+++ kotus-fo_create_lexem.py
@@ -101,19 +101,5 @@ def createlexemes(df):
                 text = text+"\t"+item
         file.write(text)
 
-    # You can easily create forms…
-    #if len(L2.forms) == 0:
-    #    L2.createForm("firsts", ["Q146786"])
-
-    # …or senses, with or without additional claims
-    #if len(L2.senses) == 0:
-    #    L2.createSense(
-    #        {
-    #            "en": "Element in an ordered list which comes before all others according to the ordering",
-    #            "de": "einer Ordnung folgend das Element vor allen anderen",
-    #        },
-    #        claims={"P5137": ["Q19269277"]},
-    #    )
-
 df = read(path+inputfile)
 createlexemes(df)
